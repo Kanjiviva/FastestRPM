61eecb6906f3dd98b3a7de63acb7a37baeacd594
Kanjiviva <stevesun0123@email.com>
2015-09-03 08:58:40 -0700

    initial commit

---
+++ FastestRPM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FastestRPM
+//
+//  Created by Steve on 2015-09-03.
+//  Copyright (c) 2015 Steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
